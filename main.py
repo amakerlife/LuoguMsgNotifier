193b82adf6c73fd04acfcf81a1fdc9408ac32a9f
Makerlife <amakerlife@outlook.com>
2023-12-24 19:03:03 +0800

    fix: a bug

--- main.py
+++ main.py
@@ -21,14 +21,6 @@ def on_open(ws):
     ws.send(data)
 
 
-def on_error(ws, error):
-    print(f"WebSocket error: {error}")
-
-
-def on_close(ws, close_status_code, close_msg):
-    print(f"WebSocket closed with status code: {close_status_code}, close message: {close_msg}")
-
-
 def on_message(ws, message):
     data = json.loads(message)
     if data.get('_ws_type') == 'server_broadcast':
@@ -57,8 +49,6 @@ if __name__ == "__main__":
     ws = websocket.WebSocketApp(ws_url,
                                 on_open=on_open,
                                 on_message=on_message,
-                                on_error=on_error,
-                                on_close=on_close,
                                 header=headers)  # 添加 header 参数
 
     print("Started")
