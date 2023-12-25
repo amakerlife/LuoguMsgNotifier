1c6dac36fec6a915f05205d3d0a664abadcf6c9d
Makerlife <amakerlife@outlook.com>
2023-12-25 20:24:59 +0800

    chore: prettier the code

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import websocket
 from win11toast import toast
 
 # 从文件中读取 _uid 和 __client_id
-with open('cookie.txt', 'r') as file:
+with open("cookie.txt", "r") as file:
     _uid, __client_id = file.read().strip().split()
 
 # 设置 WebSocket 的 headers，包含 cookie
@@ -23,22 +23,23 @@ def on_open(ws):
 
 def on_message(ws, message):
     data = json.loads(message)
-    if data.get('_ws_type') == 'server_broadcast':
-        msg = data['message']
+    if data.get("_ws_type") == "server_broadcast":
+        msg = data["message"]
         # 打印消息内容
-        print(f"{msg['sender']['name']} → {msg['receiver']['name']}: {msg['content']}")
+        print(f'{msg["sender"]["name"]} → {msg["receiver"]["name"]}: {msg["content"]}')
         button_open = {
-            'activationType': 'protocol',
-            'arguments': f"https://www.luogu.com.cn/chat?uid={msg['sender']['uid']}",
-            'content': '查看私信'
+            "activationType": "protocol",
+            "arguments": f'https://www.luogu.com.cn/chat?uid={msg["sender"]["uid"]}',
+            "content": "查看私信"
         }
         # 发送桌面通知
-        toast('收到新洛谷的私信', f"{msg['sender']['name']}: {msg['content']}",
-              duration='short',
-              icon=f"https://cdn.luogu.com.cn/upload/usericon/{msg['sender']['uid']}.png",
-              buttons=[button_open, '忽略'],
-              audio={'silent': 'true'})
-        print(f"https://cdn.luogu.com.cn/upload/usericon/{msg['sender']['uid']}.png")
+        toast("收到新洛谷的私信", f'{msg["sender"]["name"]}: {msg["content"]}',
+              duration="short",
+              icon=f'https://cdn.luogu.com.cn/upload/usericon/{msg["sender"]["uid"]}.png',
+              buttons=[button_open, "忽略"],
+              audio={"silent": "true"})
+        print(f'https://cdn.luogu.com.cn/upload/usericon/{msg["sender"]["uid"]}.png')
+
 
 if __name__ == "__main__":
     ws_url = "wss://ws.luogu.com.cn/ws"
