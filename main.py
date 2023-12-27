a883634d89c15d8b1f7c2952a7e3c1c706aad588
Makerlife <amakerlife@outlook.com>
2023-12-27 21:49:49 +0800

    feat: auto reconnect for 5 times

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import json
 import websocket
 from win11toast import toast
+import time
 
-# 从文件中读取 _uid 和 __client_id
 with open("cookie.txt", "r") as file:
     _uid, __client_id = file.read().strip().split()
 
-# 设置 WebSocket 的 headers，包含 cookie
 headers = {
     "Cookie": f"_uid={_uid}; __client_id={__client_id}"
 }
 
+cnt_reconnect = 0
+MAX_RECONNECTS = 5
+
 
 def on_open(ws):
+    global cnt_reconnect
+    print("连接成功")
+    cnt_reconnect = 0
     data = json.dumps({
         "channel": "chat",
         "channel_param": _uid,
@@ -21,32 +26,51 @@ def on_open(ws):
     ws.send(data)
 
 
+def on_close(ws, close_status_code, close_msg):
+    print("连接已被关闭")
+
+
 def on_message(ws, message):
     data = json.loads(message)
+    print(data["message"])
     if data.get("_ws_type") == "server_broadcast":
         msg = data["message"]
-        # 打印消息内容
         print(f'{msg["sender"]["name"]} → {msg["receiver"]["name"]}: {msg["content"]}')
         button_open = {
             "activationType": "protocol",
             "arguments": f'https://www.luogu.com.cn/chat?uid={msg["sender"]["uid"]}',
             "content": "查看私信"
         }
-        # 发送桌面通知
         toast("收到新的洛谷私信", f'{msg["sender"]["name"]}: {msg["content"]}',
               duration="short",
-              #icon=f'https://cdn.luogu.com.cn/upload/usericon/{msg["sender"]["uid"]}.png',
+              # icon=f'https://cdn.luogu.com.cn/upload/usericon/{msg["sender"]["uid"]}.png',
               buttons=[button_open, "忽略"],
               audio={"silent": "true"})
-        print(f'https://cdn.luogu.com.cn/upload/usericon/{msg["sender"]["uid"]}.png')
 
 
-if __name__ == "__main__":
+def connect():
+    global cnt_reconnect
     ws_url = "wss://ws.luogu.com.cn/ws"
     ws = websocket.WebSocketApp(ws_url,
                                 on_open=on_open,
                                 on_message=on_message,
-                                header=headers)  # 添加 header 参数
+                                on_close=on_close,
+                                header=headers)
+    while True:
+        ws.run_forever()
+        try:
+            ws.close()
+        except:
+            pass
+        cnt_reconnect += 1
+        print("连接已被关闭")
+        print(f'正在尝试重连（{cnt_reconnect}/{MAX_RECONNECTS}）')
+        time.sleep(5)
+        if cnt_reconnect >= MAX_RECONNECTS:
+            print("连接超时")
+            toast("连接超时")
+            break
 
-    print("Started")
-    ws.run_forever()
+
+if __name__ == "__main__":
+    connect()
